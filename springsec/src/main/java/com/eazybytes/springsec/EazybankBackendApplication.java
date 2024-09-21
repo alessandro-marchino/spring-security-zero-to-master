ad1415731442fad258244d88123cd17f3e8f3918
amarchino <marchino.alessandro@gmail.com>
2024-09-21 15:29:35 +0200

    Lesson 5

--- springsec/src/main/java/com/eazybytes/springsec/EazybankBackendApplication.java
+++ springsec/src/main/java/com/eazybytes/springsec/EazybankBackendApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringsecApplication {
+public class EazybankBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringsecApplication.class, args);
+		SpringApplication.run(EazybankBackendApplication.class, args);
 	}
 
 }
