e0d09ee4f067cd1edb345cc189c773bcc130104b
MARCHINO Alessandro 2154 <alessandro.marchino@csi.it>
2024-09-30 12:51:12 +0200

    Lesson 31

--- springsec/src/main/resources/sql/scripts.sql
+++ springsec/src/main/resources/sql/scripts.sql
@@ -18,3 +18,15 @@ INSERT IGNORE INTO `authorities` VALUES ('user', 'read');
 -- EazyBytes@54321
 INSERT IGNORE INTO `users` VALUES ('admin', '{bcrypt}$2a$12$/9LpqpmFCX1sptatVAWphO78c8cjnmv0XcNr.ioS.g70QUtDyBAb.', '1');
 INSERT IGNORE INTO `authorities` VALUES ('admin', 'admin');
+
+CREATE TABLE customer (
+	id INT NOT NULL AUTO_INCREMENT,
+	email VARCHAR(200) NOT NULL,
+	pwd VARCHAR(200) NOT NULL,
+	`role` VARCHAR(45) NOT NULL,
+	PRIMARY KEY(id)
+);
+
+INSERT INTO customer(email, pwd, `role`) VALUES ('happy@example.com', '{noop}EazyBytes@12345', 'read');
+-- EazyBytes@54321
+INSERT INTO customer(email, pwd, `role`) VALUES ('admin@example.com', '{bcrypt}$2a$12$/9LpqpmFCX1sptatVAWphO78c8cjnmv0XcNr.ioS.g70QUtDyBAb.', 'admin');
