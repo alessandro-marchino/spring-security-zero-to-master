fb179bd908a85a3d37d3fef1663098b7c4cf54bf
MARCHINO Alessandro 2154 <alessandro.marchino@csi.it>
2024-10-11 14:03:20 +0200

    Lesson 50

--- springsec/src/main/java/com/eazybytes/springsec/config/EazyBankProdUsernamePwdAuthenticationProvider.java
+++ springsec/src/main/java/com/eazybytes/springsec/config/EazyBankProdUsernamePwdAuthenticationProvider.java
@@ -2,28 +2,35 @@ package com.eazybytes.springsec.config;
 
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import lombok.RequiredArgsConstructor;
 
 @Component
 @RequiredArgsConstructor
-@Profile({ "!prod" })
-public class EazyBankUsernamePwdAuthenticationProvider implements AuthenticationProvider {
+@Profile({ "prod" })
+public class EazyBankProdUsernamePwdAuthenticationProvider implements AuthenticationProvider {
 
 	private final UserDetailsService userDetailsService;
+	private final PasswordEncoder passwordEncoder;
 
 	@Override
 	public Authentication authenticate(Authentication authentication) throws AuthenticationException {
 		String username = authentication.getName();
 		String pwd = authentication.getCredentials().toString();
 		UserDetails userDetails = userDetailsService.loadUserByUsername(username);
-		return new UsernamePasswordAuthenticationToken(username, pwd, userDetails.getAuthorities());
+		if(passwordEncoder.matches(pwd, userDetails.getPassword())) {
+			// Fetch age and perform validation to check if age is greater than 18
+			return new UsernamePasswordAuthenticationToken(username, pwd, userDetails.getAuthorities());
+		}
+		throw new BadCredentialsException("Invalid username or password");
 	}
 
 	@Override
