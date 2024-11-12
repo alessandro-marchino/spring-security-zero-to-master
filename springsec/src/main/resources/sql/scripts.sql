48ee324f1c5697cf33501d327b2364ea72854987
MARCHINO Alessandro 2154 <alessandro.marchino@csi.it>
2024-11-12 11:03:14 +0100

    Lesson 92

--- springsec/src/main/resources/sql/scripts.sql
+++ springsec/src/main/resources/sql/scripts.sql
@@ -171,7 +171,5 @@ CREATE TABLE `authorities` (
   CONSTRAINT `authorities_ibfk_1` FOREIGN KEY(`customer_id`) REFERENCES `customer`(`customer_id`)
 );
 
-INSERT INTO `authorities`(`customer_id`, `name`) VALUES (1, 'VIEWACCOUNT');
-INSERT INTO `authorities`(`customer_id`, `name`) VALUES (1, 'VIEWCARDS');
-INSERT INTO `authorities`(`customer_id`, `name`) VALUES (1, 'VIEWLOANS');
-INSERT INTO `authorities`(`customer_id`, `name`) VALUES (1, 'VIEWBALANCE');
+INSERT INTO `authorities`(`customer_id`, `name`) VALUES (1, 'ROLE_USER');
+INSERT INTO `authorities`(`customer_id`, `name`) VALUES (1, 'ROLE_ADMIN');
