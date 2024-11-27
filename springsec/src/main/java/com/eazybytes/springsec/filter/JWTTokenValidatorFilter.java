729381688ad4a02e73051196434a41ee20001eea
MARCHINO Alessandro 2154 <alessandro.marchino@csi.it>
2024-11-27 12:30:32 +0100

    Lesson 104

--- springsec/src/main/java/com/eazybytes/springsec/filter/JWTTokenValidatorFilter.java
+++ springsec/src/main/java/com/eazybytes/springsec/filter/JWTTokenValidatorFilter.java
@@ -1,9 +1,24 @@
 package com.eazybytes.springsec.filter;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
+import javax.crypto.SecretKey;
+
+import org.springframework.core.env.Environment;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import com.eazybytes.springsec.constants.ApplicationConstants;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtParser;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -13,7 +28,25 @@ public class JWTTokenValidatorFilter extends OncePerRequestFilter {
 
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		String jwt = request.getHeader(ApplicationConstants.JWT_HEADER);
+		if(jwt != null) {
+			try {
+				Environment env = getEnvironment();
+				String jwtSecret = env.getProperty(ApplicationConstants.JWT_SECRET, ApplicationConstants.JWT_SECRET_DEFAULT_VALUE);
+				SecretKey secretKey = Keys.hmacShaKeyFor(jwtSecret.getBytes(StandardCharsets.UTF_8));
+				
+				JwtParser jwtParser = Jwts.parser().verifyWith(secretKey).build();
+				Claims claims = jwtParser.parseSignedClaims(jwt).getPayload();
+				String username = claims.get("username", String.class);
+				String authorities = claims.get("authorities", String.class);
+				
+				Authentication authentication = new UsernamePasswordAuthenticationToken(username, null, AuthorityUtils.commaSeparatedStringToAuthorityList(authorities));
+				SecurityContextHolder.getContext().setAuthentication(authentication);
+			}catch(Exception e) {
+				throw new BadCredentialsException("Invalid Token received");
+			}
+		}
+		filterChain.doFilter(request, response);
 	}
 
 	@Override
