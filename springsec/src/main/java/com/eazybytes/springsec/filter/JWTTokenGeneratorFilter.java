07da286c76b5ef102363d199ee93a03a7aae7b92
MARCHINO Alessandro 2154 <alessandro.marchino@csi.it>
2024-11-27 12:12:01 +0100

    Lesson 103

--- springsec/src/main/java/com/eazybytes/springsec/filter/JWTTokenGeneratorFilter.java
+++ springsec/src/main/java/com/eazybytes/springsec/filter/JWTTokenGeneratorFilter.java
@@ -1,9 +1,22 @@
 package com.eazybytes.springsec.filter;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.stream.Collectors;
 
+import javax.crypto.SecretKey;
+
+import org.springframework.core.env.Environment;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import com.eazybytes.springsec.constants.ApplicationConstants;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -13,7 +26,24 @@ public class JWTTokenGeneratorFilter extends OncePerRequestFilter {
 
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		if(authentication != null) {
+			Environment env = getEnvironment();
+			String jwtSecret = env.getProperty(ApplicationConstants.JWT_SECRET, ApplicationConstants.JWT_SECRET_DEFAULT_VALUE);
+			SecretKey secretKey = Keys.hmacShaKeyFor(jwtSecret.getBytes(StandardCharsets.UTF_8));
+			String jwt = Jwts.builder()
+				.issuer("Eazy Bank")
+				.subject("JWT Token")
+				.claim("username", authentication.getName())
+				.claim("authorities", authentication.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.joining(",")))
+				.issuedAt(new Date())
+				// 8 hours
+				.expiration(new Date(new Date().getTime() + 28800000))
+				.signWith(secretKey)
+				.compact();
+			response.addHeader(ApplicationConstants.JWT_HEADER, jwt);
+		}
+		filterChain.doFilter(request, response);
 	}
 
 	@Override
