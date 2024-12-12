a6796aecbf4cd80de2ec5b223ac00ba5daa31262
MARCHINO Alessandro 2154 <alessandro.marchino@csi.it>
2024-12-12 15:18:17 +0100

    Lesson 130

---
+++ springsecoauth2/src/main/java/com/eazybytes/springsecoauth2/Springsecoauth2Application.java
@@ -0,0 +1,13 @@
+package com.eazybytes.springsecoauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springsecoauth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springsecoauth2Application.class, args);
+	}
+
+}
