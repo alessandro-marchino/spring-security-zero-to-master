0736175f3d7de5e6e01073c3d13707bc85ac21a7
MARCHINO Alessandro 2154 <alessandro.marchino@csi.it>
2024-12-13 09:14:34 +0100

    Lesson 133

--- springsecoauth2/src/main/java/com/eazybytes/springsecoauth2/controller/SecureController.java
+++ springsecoauth2/src/main/java/com/eazybytes/springsecoauth2/controller/SecureController.java
@@ -1,13 +1,24 @@
 package com.eazybytes.springsecoauth2.controller;
 
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Controller
+@Slf4j
 public class SecureController {
 
   @GetMapping("/secure")
-  public String securePage() {
+  public String securePage(Authentication authentication) {
+    if(authentication instanceof UsernamePasswordAuthenticationToken token) {
+      log.warn("Token: {}", token);
+    } else if (authentication instanceof OAuth2AuthenticationToken token) {
+      log.warn("Token: {}", token);
+    }
     return "secure.html";
   }
 }
